1acf1d09c99d405596ec1e19c690847e117dfa55
Akshit Garg <136995501+gargakshit001@users.noreply.github.com>
2023-06-25 22:48:51 +0530

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😉",
+    name: "Smiley winking with one eye",
+    meaning:
+      "A winking emoji indicates someone is being mischievous or humorous. It shows that someone is joking, flirting, or has a hidden agenda."
+  },
+  {
+    id: 5,
+    emoji: "😎",
+    name: "Face With Sunglasses",
+    meaning:
+      "Smart and casual, this emoji features a face with a wide, closed smile and black sunglasses. It expresses coolness and confidence and is used to convey a carefree, relaxed attitude or to express that something is excellent"
+  },
+  {
+    id: 6,
+    emoji: "🤬",
+    name: "Face With Symbols Over The Mouth",
+    meaning:
+      "The symbols over the mouth represent cursing. The serious-looking smiley is really upset and keeps using four-letter words. Represents a sudden outburst of fury or frustration."
+  }
+];
+
+export default emojipedia;
